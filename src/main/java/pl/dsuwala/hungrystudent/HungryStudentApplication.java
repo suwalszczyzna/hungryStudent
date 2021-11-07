271d9b53a2ffdb5af6a247b705c278d1e99773c8
Damian Suwała <damian.suwala@gmail.com>
2021-11-07 17:29:44 +0100

    init

---
+++ src/main/java/pl/dsuwala/hungrystudent/HungryStudentApplication.java
@@ -0,0 +1,13 @@
+package pl.dsuwala.hungrystudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HungryStudentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HungryStudentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/dsuwala/hungrystudent/HungryStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dsuwala.hungrystudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HungryStudentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
